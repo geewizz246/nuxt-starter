8b88981ac5a1fd93776a623b02479ee0229a5e8a
Daniel Brathwaite <35780932+geewizz246@users.noreply.github.com>
2025-08-24 20:05:36 -0400

    Updated to nuxt 4; use @nuxt/ui instead of shadcn; remove unnecessary libraries for starter.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,39 @@
 // @ts-check
-import eslintPrettierRecommended from "eslint-plugin-prettier/recommended";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-
+import antfu from "@antfu/eslint-config";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
-  eslintPrettierRecommended,
-  {
-    name: "geewizz246/nuxt",
-    plugins: {
-      "simple-import-sort": simpleImportSort,
+  antfu({
+    stylistic: {
+      quotes: "double",
+      semi: true,
     },
+
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": "off",
+      "antfu/if-newline": "off",
       "vue/attribute-hyphenation": "off",
-      "simple-import-sort/imports": "warn",
-      "simple-import-sort/exports": "warn",
+      "ts/no-redeclare": "off",
     },
-  },
+  }),
   {
-    name: "geewizz246/nuxt/nested-page-components",
-    files: ["{app/,}pages/**/components/**/*{server,client,}.{vue,jsx,tsx}"],
+    plugins: {
+      "better-tailwindcss": eslintPluginBetterTailwindcss,
+    },
     rules: {
-      "vue/no-multiple-template-root": "off",
+      // enable all recommended rules to report a warning
+      ...eslintPluginBetterTailwindcss.configs["recommended-warn"].rules,
+      // enable all recommended rules to report an error
+      ...eslintPluginBetterTailwindcss.configs["recommended-error"].rules,
+
+      // or configure rules individually
+      "better-tailwindcss/enforce-consistent-line-wrapping": ["warn", { printWidth: 180 }],
+    },
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "app/assets/css/main.css",
+      },
     },
   },
 );
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,65 +1,14 @@
-import type { NuxtPage } from "nuxt/schema";
-import { z } from "zod";
-
-const env = z
-  .object({
-    HELLO_WORLD: z.string().default("Hello world!"),
-  })
-  .parse(process.env);
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/image"],
 
-  css: ["assets/css/tailwind.css", "assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
 
-  runtimeConfig: {
-    public: {
-      helloWorld: env.HELLO_WORLD,
+  eslint: {
+    config: {
+      standalone: false,
     },
-  },
-
-  components: [
-    {
-      path: "~/components",
-      pathPrefix: false,
-    },
-    {
-      path: "~/pages",
-      pattern: ["**/components/**"],
-      pathPrefix: false,
-    },
-  ],
-
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "shadcn-nuxt",
-    "radix-vue/nuxt",
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxtjs/color-mode",
-    "dayjs-nuxt",
-  ],
-
-  hooks: {
-    "pages:extend": ignoreComponentsDirInPages,
-  },
-
-  colorMode: {
-    classSuffix: "",
   },
 });
-
-/** Do not treat components created in a directory named "components" as pages within the "pages" directory. */
-function ignoreComponentsDirInPages(pages: NuxtPage[]) {
-  const pagesToRemove = pages.filter((page) => page.path.includes("component"));
-
-  pagesToRemove.forEach((page) => pages.splice(pages.indexOf(page), 1));
-}
